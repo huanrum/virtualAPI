1d15ae815f2e54b36af26c39a767f52edf65160d
seto.sun <seto.sun@sinodynamic.com>
2017-04-25 14:46:33 +0800

    charset

--- index.js
+++ index.js
@@ -4,45 +4,48 @@ var random = require('./random');
 var config = JSON.parse(fs.readFileSync('config.json').toString());
 
 http.createServer(function (request, response) {
-
-    // 发送 HTTP 头部 
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    if (!Object.keys(config).some(function (key) {
-        var method = key.match(/\[(.*)\]/) && key.match(/\[(.*)\]/)[1];
-        var keys = key.replace(/\[.*\]/,'').replace(/\s*\//, '').replace(/\/\s*$/, '').replace(/\/\//g,'/').split('/');
-        var urls = request.url.split('?')[0].replace(/\s*\//, '').replace(/\/\s*$/, '').replace(/\/\//g,'/').split('/');
-        if (keys.length === urls.length && (!method || method === request.method)) {
-            var find = true;
-            // 解析url获取参数
-            var parameters = {};
-            if (request.url.split('?')[1]) {
-                request.url.split('?')[1].split('&').forEach(function (str) {
-                    parameters[str.split('=')[0]] = str.split('=')[1];
-                });
-            }
-            for (var i = 0; i < keys.length; i++) {
-                if (keys[i] !== urls[i] && !/:.*/.test(keys[i])) {
-                    find = false;
-                } else if (/:.*/.test(keys[i])) {
-                    parameters[keys[i].replace(':', '')] = urls[i];
+    try {
+        if (!Object.keys(config).some(function (key) {
+            var method = key.match(/\[(.*)\]/) && key.match(/\[(.*)\]/)[1];
+            var keys = key.replace(/\[.*\]/, '').replace(/\s*\//, '').replace(/\/\s*$/, '').replace(/\/\//g, '/').split('/');
+            var urls = request.url.split('?')[0].replace(/\s*\//, '').replace(/\/\s*$/, '').replace(/\/\//g, '/').split('/');
+            if (keys.length === urls.length && (!method || method === request.method)) {
+                var find = true;
+                // 解析url获取参数
+                var parameters = {};
+                if (request.url.split('?')[1]) {
+                    request.url.split('?')[1].split('&').forEach(function (str) {
+                        parameters[str.split('=')[0]] = str.split('=')[1];
+                    });
                 }
-            }
-            if (find) {
-                var configStr = JSON.stringify(config[key]);
-                Object.keys(parameters).forEach(function(parm){
-                    configStr = configStr.replace(new RegExp(':'+parm,'g'),parameters[parm]);
-                });
-                response.end(JSON.stringify(random(JSON.parse(configStr))));
-                return true;
-            }
+                for (var i = 0; i < keys.length; i++) {
+                    if (keys[i] !== urls[i] && !/:.*/.test(keys[i])) {
+                        find = false;
+                    } else if (/:.*/.test(keys[i])) {
+                        parameters[keys[i].replace(':', '')] = urls[i];
+                    }
+                }
+                if (find) {
+                    var configStr = JSON.stringify(config[key]);
+                    Object.keys(parameters).forEach(function (parm) {
+                        configStr = configStr.replace(new RegExp(':' + parm, 'g'), parameters[parm]);
+                    });
 
-        }
-    })) {
-        response.end('404');
-    };
+                    response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                    response.end(JSON.stringify(random(JSON.parse(configStr))));
+                    return true;
+                }
+
+            }
+        })) {
+            response.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' });
+            response.end('<strong>404</strong> 地址不存在');
+        };
+    } catch (e) {
+        response.writeHead(500, { 'Content-Type': 'text/html;charset=utf-8' });
+        response.end('<strong>500</strong> 服务器异常' + e.message);
+    }
+
 
 }).listen(8888);
 
