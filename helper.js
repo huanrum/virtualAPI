8e8f7a2c979ce553f032955e7e5ce2ca38280cb0
seto.sun <seto.sun@sinodynamic.com>
2017-04-26 16:31:08 +0800

    add home page

--- helper.js
+++ helper.js
@@ -4,48 +4,58 @@ var fs = require("fs");
 
 module.exports = (function () {
 
-    var config = initConfig('config');
+    var returnData = {};
+    var helpHtml = fs.readFileSync('help.html').toString().replace('window.configData = []', 'window.configData = '+ JSON.stringify(initConfig('config',returnData)));
 
-    return function (request) {
-
-        var keys = Object.keys(config);
+    return function (request,bodyData) {
+        if(!request){
+            return helpHtml;
+        }
+        var keys = Object.keys(returnData);
         for (var index = 0; index < keys.length; index++) {
             //判断是否是对应的url
             if ((!keys[index].match(/\[(.*)\]/) || new RegExp('^\[' + request.method + '\]', 'i').test(keys[index]))
                 && new RegExp('^\\/*' + keys[index].replace(/\//g, '\\/+').replace(/:((?!\/).)*/g, '((?!\\/).)+') + '\\/*$').test(request.url.split('?')[0])) {
                 var parameters = getParameters(keys[index], request.url.split('?'));
-                var configStr = JSON.stringify(config[keys[index]]);
+                var configStr = JSON.stringify(returnData[keys[index]]);
                 Object.keys(parameters).forEach(function (parm) {
                     configStr = configStr.replace(new RegExp(':' + parm, 'g'), parameters[parm]);
+                });
+                Object.keys(bodyData).forEach(function (parm) {
+                    configStr = configStr.replace(new RegExp(':' + parm, 'g'), bodyData[parm]);
                 });
                 return JSON.parse(configStr);
             }
         }
     };
 
-    function initConfig(dirpath,config){
-        config = config || {};
-      fs.readdirSync(dirpath).forEach(function(item){
+    function initConfig(dirpath, returnData) {
+        var config = [];
+        fs.readdirSync(dirpath).forEach(function (item) {
             var info = fs.statSync(dirpath + '/' + item);
-			if (info.isDirectory()) {
-				initConfig(dirpath + '/' + item ,config);
-			} else {
-				var data = JSON.parse(fs.readFileSync(dirpath + '/' + item).toString());
-                Object.keys(data).forEach(function(key){
-                    config[key] = data[key];
+            if (info.isDirectory()) {
+                config = config.concat(initConfig(dirpath + '/' + item, returnData));
+            } else if (/\.json$/.test(item)) {
+                var data = JSON.parse(fs.readFileSync(dirpath + '/' + item).toString());
+                 config.push({
+                    file: dirpath + '/' + item,
+                    config: data
                 });
-			}
-      });
-      return config;
+                Object.keys(data).forEach(function (key) {
+                    returnData[key] = data[key];
+                });
+            }
+        });
+        return config;
     }
 
     // 解析url获取参数
     function getParameters(key, urlAndParms) {
         var parameters = {};
 
-        var keys = key.replace(/\[.*\]/, '').split('/').filter(function(i){return !!i;});
-        var urls = urlAndParms[0].split('/').filter(function(i){return !!i;});
-        
+        var keys = key.replace(/\[.*\]/, '').split('/').filter(function (i) { return !!i; });
+        var urls = urlAndParms[0].split('/').filter(function (i) { return !!i; });
+
         if (urlAndParms[1]) {
             urlAndParms[1].split('&').forEach(function (str) {
                 parameters[str.split('=')[0]] = str.split('=')[1];
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var fs = require("fs");
+var querystring = require('querystring');
 
 var random = require('./random');
 var helper = require('./helper');
@@ -6,21 +8,31 @@ var helper = require('./helper');
 
 http.createServer(function (request, response) {
     response.setHeader('Access-Control-Allow-Origin', '*')
-    try {
-        var returnData = helper(request);
-        if (returnData) {
-            response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
-            response.end(JSON.stringify(random(returnData)));
-        } else {
-            response.writeHead(404, { 'Content-Type': 'text/plain;charset=utf-8' });
-            response.end('404 地址不存在');
+    if (/^[\/\s]*$/.test(request.url)){
+        response.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+        response.end(helper());
+    } else {
+        try {
+            var bodyData = '';
+            request.on('data', function (chunk) {
+                bodyData += chunk;
+            });
+            request.on('end', function () {
+                var returnData = helper(request, querystring.parse(bodyData));
+                if (returnData) {
+                    response.writeHead(200, { 'Content-Type': 'text/plain;charset=utf-8' });
+                    response.end(JSON.stringify(random(returnData)));
+                } else {
+                    response.writeHead(404, { 'Content-Type': 'text/plain;charset=utf-8' });
+                    response.end('404 地址不存在');
+                }
+            });
+
+        } catch (e) {
+            response.writeHead(500, { 'Content-Type': 'text/plain;charset=utf-8' });
+            response.end('500 服务器异常' + e.message);
         }
-    } catch (e) {
-        response.writeHead(500, { 'Content-Type': 'text/plain;charset=utf-8' });
-        response.end('500 服务器异常' + e.message);
     }
-
-
 }).listen(8888);
 
 // 终端打印如下信息
