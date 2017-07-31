f1907db0b90840805b35b7c235712052a1ddd81a
seto.sun <seto.sun@sinodynamic.com>
2017-07-31 11:54:02 +0800

    update

---
+++ config/tng-Mobile/login.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+var tempData = {pushEnable:"false"};
+
+
+
+module.exports = {
+    "/tngMobile/consumer/get-consumer-by-id": function(data,parameters,bodyData){
+        parameters.icon_id = eval(parameters.consumer_id.split().map(i=>i.charCodeAt()).join('+')) %7 + 1;
+        return data;
+    },
+    "tngMobile/takePhoto":function(data,parameters,bodyData){
+        if(!parameters.encode){
+            return 'http://127.0.0.1:8888/images/takePhoto.png';
+        }else{
+            return new Buffer(fs.readFileSync('images/takePhoto.png')).toString('base64');
+        }
+    }
+}
