e5dd5cb170fc69674af7db659b0e42603ff8b853
seto.sun <seto.sun@sinodynamic.com>
2018-02-27 10:36:21 +0800

    update to webservice

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var http = require('http');
-var child_process = require('child_process');
-
-
-var bin = require('./bin');
-
-bin({
+require('./bin')({
     port:8888,
     websocket:8800 + Math.floor(Math.random() * 44),
     weinre:8844 + Math.floor(Math.random() * 44)
