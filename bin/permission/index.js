76f38d08ffa70b66ac0d2746e360a2d0554e1eaf
seto.sun <seto.sun@sinodynamic.com>
2018-03-02 16:07:00 +0800

    update

--- bin/permission/index.js
+++ bin/permission/index.js
@@ -1,20 +1,29 @@
+var web = require('./web');
 
+module.exports = (function () {
+    var configs = [];
 
-module.exports = (function(){
-    var permission = [undefined,''];
-
-    return function action(request,response){
-        return new Promise(succ => {
-            succ(); 
-            // if(permission.indexOf(request.headers.token) !== -1){
-            //     succ(); 
-            // }else{
-            //     succ({
-            //         code:403,
-            //         message:'没有权限'
-            //     });
-            // }
-        });
+    permission.config = function () {
+        configs.push(fn);
     };
+
+    return permission;
+
+    function permission(request, response) {
+        if (web(request)) {
+            return Promise.resolve();
+        } else {
+            var promises = configs.map(f => f(request, response, bodyData)).filter(i => !!i);
+            if (promises.length) {
+                return Promise.all(promises);
+            } else {
+                return Promise.resolve({
+                    code: 403,
+                    message: '没有权限'
+                });
+            }
+        }
+
+    }
 
 })();
---
+++ bin/permission/web/index.js
@@ -0,0 +1,54 @@
+var helper = require('./../../helper');
+var resource = require('./../../resource');
+
+module.exports = (function(){
+
+    return function(request){
+        if (/^[\/\s]*$/.test(request.url)) {
+            return true;
+        }
+        //测试API
+        else if (/^\/*test\/+/.test(request.url)) {
+            return true;
+        }
+        else if (/^\/*framework\/+/.test(request.url)) {
+            return true;
+        }
+        //展示对应的视图
+        else if (/^\/*views\/*/i.test(request.url)) {
+            return true;
+        }
+        //用于模拟手机APP处理缓存数据
+        else if (/^\/*cache\/*/i.test(request.url)) {
+            return true;
+        }
+        //用于处理数据库相关数据
+        else if (/^\/*db\/*/i.test(request.url)) {
+            return true;
+        }
+        //用于处理其他域名下的API请求
+        else if (/^\/*proxy\/*/i.test(request.url)) {
+            return true;
+        }
+        //对请求的数据处理后再返回
+        else if (/^\/*random/.test(request.url)) {
+            return true;
+        }
+        //配置Web
+        else if (/^\/*config\/+/.test(request.url)) {
+            return helper.getClientIp(request).indexOf(helper.netInfo().address) !== -1;
+        }
+        //WEB操作控制服务端
+        else if (/^\/*action\/+/i.test(request.url)) {
+            return helper.getClientIp(request).indexOf(helper.netInfo().address) !== -1;
+        }
+        //资源文件
+        else if (resource.test(request.url)) {
+            return true;
+        }
+        else {
+            return true;
+        }
+    };
+
+})();
