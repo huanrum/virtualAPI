1810e428a3253bd2f079a088a5abcc0615805cec
seto.sun <seto.sun@sinodynamic.com>
2018-04-10 16:25:02 +0800

    update

--- bin/websocket/index.js
+++ bin/websocket/index.js
@@ -5,7 +5,10 @@ var helper = require('./../helper');
 module.exports = function (options) {
 
     helper.initModule('ws').then(ws => {
-        var wss = new ws.Server({ port: options.websocket }), wList = {};
+        var wss = new ws.Server({
+                port: options.websocket
+            }),
+            wList = {};
 
         var send = function (w, data) {
             try {
@@ -18,48 +21,54 @@ module.exports = function (options) {
         wss.on('connection', function (client) {
             client.on('message', function (message) {
                 var messageData = JSON.parse(message);
-                var clientId = Object.keys(wList).find(function (i) { return client === wList[i]; });
-                switch (messageData.action) {
-                    case 'login':
-                        //登录
-                        clientId = messageData.data;
-                        Object.keys(wList).forEach(function (w) {
-                            send(w, {
-                                action: messageData.action,
-                                from: clientId
-                            });
-                        });
-                        client.send(JSON.stringify({
-                            action: messageData.action,
-                            data: Object.keys(wList)
-                        }));
-                        wList[clientId] = client;
-                        break;
-                    case 'logout':
-                        //登出
-                        Object.keys(wList).forEach(function (w) {
-                            if (w === clientId) {
-                                delete wList[w];
-                            } else {
+                var clientId = Object.keys(wList).find(function (i) {
+                    return client === wList[i];
+                });
+                try {
+                    switch (messageData.action) {
+                        case 'login':
+                            //登录
+                            clientId = messageData.data;
+                            Object.keys(wList).forEach(function (w) {
                                 send(w, {
                                     action: messageData.action,
                                     from: clientId
                                 });
-                            }
-                        });
-                        break;
-                    default:
-                        //传递消息
-                        Object.keys(wList).forEach(function (w) {
-                            if (wList[w] !== client && (!messageData.to || messageData.to.indexOf(w) !== -1)) {
-                                send(w, {
-                                    action: messageData.action,
-                                    from: clientId,
-                                    data: messageData.data
-                                });
-                            }
-                        });
-                        break;
+                            });
+                            client.send(JSON.stringify({
+                                action: messageData.action,
+                                data: Object.keys(wList)
+                            }));
+                            wList[clientId] = client;
+                            break;
+                        case 'logout':
+                            //登出
+                            Object.keys(wList).forEach(function (w) {
+                                if (w === clientId) {
+                                    delete wList[w];
+                                } else {
+                                    send(w, {
+                                        action: messageData.action,
+                                        from: clientId
+                                    });
+                                }
+                            });
+                            break;
+                        default:
+                            //传递消息
+                            Object.keys(wList).forEach(function (w) {
+                                if (wList[w] !== client && (!messageData.to || messageData.to.indexOf(w) !== -1)) {
+                                    send(w, {
+                                        action: messageData.action,
+                                        from: clientId,
+                                        data: messageData.data
+                                    });
+                                }
+                            });
+                            break;
+                    }
+                } catch (e){ 
+                
                 }
             }).on('error', function (e) {
 
