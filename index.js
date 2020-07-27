73e2e76c61a000561c4d8f10d95f0dee1d88991e
ehuanrum <huanrum@126.com>
2020-07-27 10:33:33 +0800

    remove some files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-require('./bin')({
-    kill:false,
-    port:8888,
-    websocket:8800 + Math.floor(Math.random() * 44),
-    weinre:8844 + Math.floor(Math.random() * 44)
-});
 
+setTimeout(()=>require('./bin/db')({method:'PUT',url:'/db/sqlite/START_INFO',body:{name:Date.now(),info:'打开服务的时间'}}),2000);
+
+require('./bin')({
+    debug:true,
+    kill:false,
+    port:process.argv[2] || 8888,
+    websocket:8844 + Math.floor(Math.random()*44),
+    weinre:8800 + Math.floor(Math.random()*44),
+    exclude:i=>/career/.test(i)
+});
